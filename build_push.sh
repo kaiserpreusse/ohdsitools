ad748cfe01e4ca98990237f2a901dc20c8677ff5
Martin Preusse <martin.preusse@gmail.com>
2022-10-07 19:32:43 +0200

    setup qemu

--- build_push.sh
+++ build_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx build --platform linux/amd64,linux/arm64 --push -t kaiserpreusse/ohdsitools_atlas:latest atlas
 docker buildx build --platform linux/amd64,linux/arm64 --push -t kaiserpreusse/ohdsitools_webapi:latest webapi
 docker buildx build --platform linux/amd64,linux/arm64 --push -t kaiserpreusse/ohdsitools_webapi_database:latest webapi_database
